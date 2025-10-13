b2f4d604faa65b290e8fd125b4d49f5a8d04c5d3
uxepd <uxepd@student.kit.edu>
2025-10-13 22:21:55 +0200

    feat: color def

--- entities/request.gd
+++ entities/request.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 var expected_product: FireworkResource
 
+# farbe array 
+# dict für farbwerte 
+# farben mischen wird in der mitte geschaut
+# farb tolleranz auf distanz im array zum farbwert
 
 func is_as_requested(final_product: FireworkResource) -> bool:
 	#
