30f3f49756269f39eb1de51845986da9b43697ee
uxepd <uxepd@student.kit.edu>
2025-10-13 21:15:27 +0200

    feat: abstract resource

---
+++ entities/Ingredients/AbstractIngredient.gd
@@ -0,0 +1,20 @@
+extends Control
+class_name AbstractIngredient
+
+@export var resourceData: FireworkResource
+@export var texture: Texture2D
+
+func _get_drag_data(_pos: Vector2) -> FireworkResource:
+	# Use another resource as drag preview.
+	var res = TextureRect.new()
+	res.texture = texture
+
+	# Allows us to center the resource on the mouse
+	var preview = Control.new()
+	preview.add_child(res)
+	res.position = -0.5 * res.size
+	# Sets what the user will see they are dragging
+	set_drag_preview(preview)
+	# Return resource as drag data
+	return resourceData;
+	
