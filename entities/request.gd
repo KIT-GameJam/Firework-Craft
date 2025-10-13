1b90cb1dcad08e145f1c3e18ddce166bcb9d097b
uxepd <uxepd@student.kit.edu>
2025-10-13 22:52:49 +0200

    feat: compare funktion oder so

--- entities/request.gd
+++ entities/request.gd
@@ -1,12 +1,26 @@
 extends Node2D
 
+const shapeTollerance: int = 1
+const colorTollerance: int = 1
+const sizeTollerance: int = 1
+
 var expected_product: FireworkResource
+var textDescription: String
+
+@onready var label: Label = $PanelContainer/MarginContainer/Label
+
+func _ready() -> void:
+	label.text = textDescription # Oder so?
 
 # farbe array 
 # dict für farbwerte 
 # farben mischen wird in der mitte geschaut
 # farb tolleranz auf distanz im array zum farbwert
 
+
 func is_as_requested(final_product: FireworkResource) -> bool:
-	#
-	return true
+	var shapeDiff: int = abs(final_product.corner_modifier - expected_product.color_modifier)
+	var colorDiff: int = abs((final_product.color_modifier - expected_product.color_modifier) % 12) # TODO Funktion überprüfen
+	var sizeDiff: int = abs(final_product.size_modifier - expected_product.color_modifier)
+	
+	return shapeDiff <= shapeTollerance && colorDiff <= colorTollerance && sizeDiff <= sizeTollerance
