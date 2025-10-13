8ab6f1a821f70f6096b0fe96641245f746e96165
Laura Blechschmidt <laurablech02@gmail.com>
2025-10-13 19:52:41 +0200

    attributes, some func of flask

--- levels/flask.gd
+++ levels/flask.gd
@@ -1,1 +1,13 @@
-extends Node
+extends Area2D
+
+var current_value := 0
+var product_costs := 0
+var flask_color := 0 # default: white
+var flask_form := 0 # default: small
+var flask_corners := 0 # default: circle
+
+func calc_values() -> void:
+	current_value = current_value + flask_color + flask_form + flask_corners
+	
+#func adapt_flask_values() -> void:
+	
--- levels/level_1.gd
+++ levels/level_1.gd
@@ -1,1 +1,16 @@
 extends Area2D
+
+var current_value := 0
+var ressource_costs := 0
+#var curr_ingredient: FireworkResource
+
+func calc_product_val() -> void:
+	var calc_val := current_value
+	
+	#calculation of resource values
+	current_value = calc_val
+	
+#get drop data	
+	
+func on_reset() -> void:
+	current_value = 0
