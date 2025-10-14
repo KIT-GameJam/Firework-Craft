958d2ed26f8ce58f90f6cc2a091866609619daae
uxepd <uxepd@student.kit.edu>
2025-10-14 11:56:49 +0200

    temp label settings

--- entities/request.gd
+++ entities/request.gd
@@ -1,16 +1,21 @@
 extends Control
 
-const shapeTollerance: int = 1
-const colorTollerance: int = 1
-const sizeTollerance: int = 1
+@export var shapeTollerance: int = 1
+@export var colorTollerance: int = 1
+@export var sizeTollerance: int = 1
+
+@export var textures: Array[Texture2D]
 
 var expected_product: FireworkResource
 var textDescription: String
 
 @onready var label: Label = $PanelContainer/MarginContainer/Label
+@onready var textureRect: TextureRect = $PanelContainer/MarginContainer/TextureRect
 
 func _ready() -> void:
 	label.text = textDescription # Oder so?
+	textureRect.texture = textures.pick_random()
+	
 
 # farbe array 
 # dict für farbwerte 
