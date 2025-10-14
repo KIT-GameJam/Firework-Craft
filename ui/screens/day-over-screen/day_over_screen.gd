593fea60b5bc993d49a92244a1a6d2c2d6b57d0b
Laura Blechschmidt <laurablech02@gmail.com>
2025-10-14 21:31:22 +0200

    added progress bar

--- ui/screens/day-over-screen/day_over_screen.gd
+++ ui/screens/day-over-screen/day_over_screen.gd
@@ -1,15 +1,24 @@
 extends Control
+
 var level_nr: int
+@onready var title : Label = $VBoxContainer/Title
+@onready var progress_bar : ProgressBar = $VBoxContainer/ProgressBar
+var total_satisfaction : int = 75 # TODO: set this
 signal select_level_1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	title.text = "Day " + str(level_nr+1)
+	progress_bar.value = 0
+	var tween = create_tween()
+	tween.tween_property(progress_bar, "value", total_satisfaction, 2.0) # (Zielwert, Dauer)
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_OUT)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-
+	
 func _on_button_pressed() -> void:
 	select_level_1.emit()
 	queue_free()
