1ac7ea35838ed3bf59dffcdcdd2bd6296b91a7f5
uxepd <uxepd@student.kit.edu>
2025-10-14 17:11:28 +0200

    colors

--- autoloads/global.gd
+++ autoloads/global.gd
@@ -2,7 +2,22 @@ extends Node
 
 var game_manager : GameManager = null
 var selected_res : AbstractIngredient = null
-var selectedReq: Request = null
+var selectedRequest: RequestResource = null
+
+const colors = {
+	1: Color('f4e601'), # yellow
+	2: Color('f9c30b'), # yellow_orange
+	3: Color('f28e1e'), # orange
+	4: Color('eb6120'), # orange_red
+	5: Color('e12323'), # red
+	6: Color('c1027b'), # purple_red
+	7: Color('6d398b'), # purple
+	8: Color('444d98'), # purple_blue
+	9: Color('2b70b1'), # blue
+	10: Color('0797bc'), # light_blue
+	11: Color('018e5a'), # green
+	12: Color('8dbb27') # olive
+}
 
 func set_game_manager(p_game_manager: GameManager):
 	game_manager = p_game_manager
