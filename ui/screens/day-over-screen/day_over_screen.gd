76f9927e1ae2e65439b1e420a27f5e3051d36e50
Laura Blechschmidt <laurablech02@gmail.com>
2025-10-15 10:57:37 +0200

    calc total satisfaction for stats

--- ui/screens/day-over-screen/day_over_screen.gd
+++ ui/screens/day-over-screen/day_over_screen.gd
@@ -10,6 +10,7 @@ signal select_level_1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	title.text = "Day " + str(level_nr)
+	calc_total_satisfaction()
 	progress_bar.value = 0
 	var tween = create_tween()
 	tween.tween_property(progress_bar, "value", total_satisfaction, 2.0) # (Zielwert, Dauer)
@@ -19,6 +20,12 @@ func _ready() -> void:
 func set_stats(stats_arr: Array[Level.CustomerStats], curr_level: int):
 	level_stats = stats_arr
 	level_nr = curr_level
+
+func calc_total_satisfaction():
+	var curr_satisfaction = 0
+	for i in range(level_stats.size()):
+		curr_satisfaction = curr_satisfaction + level_stats.get(i).satisfaction
+	total_satisfaction = curr_satisfaction	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
