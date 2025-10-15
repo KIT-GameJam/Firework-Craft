059a6719e084afc504b61859db4b7756a0ddafa8
Ekaterina <ekaterinaehringhaus180@gmail.com>
2025-10-15 12:51:26 +0200

    Add Difficulty curve to Levels

--- gamemanager/level_manager.gd
+++ gamemanager/level_manager.gd
@@ -6,6 +6,9 @@ var level_scene: PackedScene = load("res://levels/main_level.tscn")
 var stats_scene: PackedScene = load("res://ui/screens/day-over-screen/day_over_screen.tscn")
 var level_number: int = 0
 
+var level_times: Array[float] = [30, 25, 25, 22, 21, 20, 20, 20, 20, 15]
+var level_request_amounts: Array[int] = [2, 3, 4, 4, 5, 6, 7, 8, 9, 10]
+
 func _ready() -> void:
 	_next_level()
 
@@ -13,9 +16,11 @@ func _next_level() -> void:
 	level_number += 1
 	_make_level(level_number)
 
-func _make_level(_nr: int) -> void:
+func _make_level(nr: int) -> void:
 	var level: Level = level_scene.instantiate()
 	level.day_over.connect(_show_stats)
+	level.requests_amount = level_request_amounts[min(nr,10)]
+	level.daylength = level_times[min(nr,10)]
 	add_child(level)
 
 func _show_stats(_v: Array[Level.CustomerStats]) -> void :
